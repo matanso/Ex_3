70fc32c4da3ec677652378bb32c914f85eb68664
DirtyLeaderz <ariael2009@gmail.com>
2017-01-26 13:53:15 +0200

    Finish siftDist

--- sp_image_proc_util.cpp
+++ sp_image_proc_util.cpp
@@ -11,6 +11,7 @@
 
 
 
+
 #define IMG_LOAD_ERR "Image cannot be loaded - %s\n"
 // #define DIVIDED_BY_THREE *0.33
 
@@ -30,8 +31,8 @@ SPPoint **spGetRGBHist(const char *str, int imageIndex, int nBins) {
     float range[] = {0, 256};
     const float *histRange = {range};
 
-	// Set the other parameters:
-	int nImages = 1;
+    // Set the other parameters:
+    int nImages = 1;
 
     //bool uniform = true; Not needed
     //bool accumulate = false; Not neneed
@@ -45,7 +46,7 @@ SPPoint **spGetRGBHist(const char *str, int imageIndex, int nBins) {
         double *data = (double *) malloc(nBins * sizeof(double));
         for (int j = 0; j < nBins; j++) data[j] = hist.data[j];
         out[i] = spPointCreate(data, nBins, imageIndex);
-    	free(data);
+        free(data);
     }
     return out;
 }
@@ -76,7 +77,7 @@ SPPoint **spGetSiftDescriptors(const char *str, int imageIndex, int nFeaturesToE
         return NULL;
     }
 
-    int dim;
+    int featuresDim;
     SPPoint** features;
 
     // Key points will be stored in kp1;
@@ -89,8 +90,8 @@ SPPoint **spGetSiftDescriptors(const char *str, int imageIndex, int nFeaturesToE
     // Extracting features
     // The features will be stored in ds1
     // The output type of ds1 is CV_32F (float)
-    detect->cv::src(detect, kp1, cv::Mat());
-    detect->cv::compute(img, kp1, ds1);
+    detect->detect(img, kp1, cv::Mat());
+    detect->compute(img, kp1, ds1);
     
     *nFeatures = ds1.rows;
     featuresDim = ds1.cols;
@@ -99,13 +100,13 @@ SPPoint **spGetSiftDescriptors(const char *str, int imageIndex, int nFeaturesToE
     if (features == NULL){ return NULL; }
 
 
-    tmp_features = (double) malloc(*nFeatures*(double));
+    double* tmp_features = (double*) malloc(*nFeatures*sizeof(*tmp_features));
     for(int i = 0; i< *nFeatures; i++){
         for (int j = 0; j < featuresDim; j++)
         {
             tmp_features[j] = ds1.at<float>(i,j);
         }
-        features[i] = spPointCreate(*tmp_features, featuresDim, imageIndex);
+        features[i] = spPointCreate(tmp_features, featuresDim, imageIndex);
     }
     free(tmp_features);
     return features;
@@ -114,20 +115,41 @@ SPPoint **spGetSiftDescriptors(const char *str, int imageIndex, int nFeaturesToE
 int *spBestSIFTL2SquaredDistance(int kClosest, SPPoint *queryFeature,
                                  SPPoint ***databaseFeatures, int numberOfImages,
                                  int *nFeaturesPerImage) {
-	// Check if queryFeature is NULL
-	if(queryFeature == NULL) { return NULL; }
-	// Check if databaseFeatures is NULL
-	if(databaseFeatures == NULL) { return NULL; }
-	// Check if nFeaturesPerImage is NULL
-	if(nFeaturesPerImage == NULL) { return NULL; }
-	// Check if numberOfImages <= 1
-	if(numberOfImages <= 1){ return NULL; }
+    // Check if queryFeature is NULL
+    if(queryFeature == NULL) { return NULL; }
+    // Check if databaseFeatures is NULL
+    if(databaseFeatures == NULL) { return NULL; }
+    // Check if nFeaturesPerImage is NULL
+    if(nFeaturesPerImage == NULL) { return NULL; }
+    // Check if numberOfImages <= 1
+    if(numberOfImages <= 1){ return NULL; }
 
-	// Create a priority queue of size kClosest
-	SPBPQueue *KClosestImages = spBPQueueCreate(kClosest);
-	if(KClosestImages == NULL){ retrun NULL; }
+    // Create a min-priority queue of size kClosest
+    SPBPQueue *KClosestImages = spBPQueueCreate(kClosest);
+    if(KClosestImages == NULL){ return NULL; }
 
+    for(int i = 0; i < numberOfImages; i++){
+        for(int j = 0; j<nFeaturesPerImage[i]; j++){
+            int dist = spPointL2SquaredDistance(databaseFeatures[i][j], queryFeature);
+            // TODO the enqueue should take to consideration the index, if 
+            // two featutes have same distance with queryFeature, then
+            // the smallest index is considered to be closer.
+            if(spBPQueueEnqueue(KClosestImages, i, dist) == SP_BPQUEUE_INVALID_ARGUMENT){
+                // KClosestImages is NULL argument
+            }
+        }
+    }
 
+    // The returned array of closest features to queryFeature
+    int* sortedImagesByFeatures = (int*) malloc(spBPQueueSize(KClosestImages)*sizeof(int));
+    BPQueueElement element;
+    for (int i = 0; i < spBPQueueSize(KClosestImages) ; i++){
+        spBPQueuePeek(KClosestImages, &element);
+        sortedImagesByFeatures[i] = element.index;
+    }
+
+    spBPQueueDestroy(KClosestImages);
+    return sortedImagesByFeatures;
 
 }
 
