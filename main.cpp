06b70807f0c94bd142d52a58a70258940ac62274
DirtyLeaderz <ariael2009@gmail.com>
2017-01-26 12:35:12 +0200

    Added new files and finished the sift discriptor

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 using namespace std;
 
 int main() {
-    SPPoint **shit = spGetRGBHist("/home/matan/SoftwareProject/Ex_3/images/img1.png", 0, 255);
-
+    SPPoint **shit = spGetRGBHist("./images/img2.jpg", 0, 255);
+    if (shit == NULL) {
+    	exit(1);
+    }
     return 0;
 }
